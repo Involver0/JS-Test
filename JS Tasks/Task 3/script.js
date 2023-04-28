80d1cbd894e578dbe74c223283a0d04d2950743e
Regimantas <regimantasberzinskas@gmail.com>
2023-04-28 15:45:26 +0300

    Task-3 Completed

--- JS Tasks/Task 3/script.js
+++ JS Tasks/Task 3/script.js
@@ -12,3 +12,46 @@ būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'https://api.github.com/users';
+
+// GET function
+
+const getInfo = async () => {
+  try {
+    const response = await fetch(ENDPOINT);
+    const data = await response.json();
+    renderInfo(data);
+  } catch (err) {
+    console.error(err);
+  }
+};
+getInfo();
+
+// Display Info
+
+const renderInfo = (array) => {
+  const output = document.getElementById('output');
+
+  if (!output && !array) {
+    return console.log('data does not exist and output is not exist');
+  }
+
+  document.getElementById('btn').addEventListener('click', () => {
+    array.forEach((user) => {
+      const loginEl = document.createElement('h3');
+      loginEl.textContent = user.login;
+
+      const avatarEl = document.createElement('img');
+      avatarEl.src = user.avatar_url;
+      avatarEl.alt = `${user.login}  photo`;
+
+      const cardEl = document.createElement('div');
+      cardEl.classList = 'card';
+      cardEl.append(avatarEl, loginEl);
+      const userSectionEl = document.getElementById('output');
+      userSectionEl.append(cardEl);
+
+      const removeEl = document.getElementById('message');
+      removeEl.classList = 'remove';
+    });
+  });
+};
