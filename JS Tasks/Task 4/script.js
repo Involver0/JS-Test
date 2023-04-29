6f34db2ee5d6c0670f059e190a895a6c7b66faf1
Regimantas <regimantasberzinskas@gmail.com>
2023-04-29 14:08:56 +0300

    Task - 4: problematic li append

--- JS Tasks/Task 4/script.js
+++ JS Tasks/Task 4/script.js
@@ -9,3 +9,40 @@ būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+const getCarsBrands = async () => {
+  try {
+    const response = await fetch('./cars.json');
+    const data = await response.json();
+    renderCarBrands(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+getCarsBrands();
+
+const renderCarBrands = (carBrands) => {
+  const output = document.getElementById('output');
+  if (!output && !carBrands) {
+    return console.log('data does not exist and output is not exist');
+  }
+  carBrands.forEach((carBrand) => {
+    for (i = 0; i <= carBrand.models.length; i++) {
+      // Element creation
+      const cardEl = document.createElement('div');
+      const headingEl = document.createElement('h3');
+      const ulEl = document.createElement('ul');
+      let liEl = document.createElement('li');
+
+      // Assigning
+      headingEl.textContent = carBrand.brand;
+      liEl.textContent = carBrand.models[i];
+      // Styling
+      cardEl.classList = 'card';
+      // Append
+      ulEl.appendChild(liEl); // nesugalvoju kaip cia su appendinti kad sudetu kelis i viena
+      cardEl.append(headingEl, ulEl);
+      output.append(cardEl);
+    }
+  });
+};
