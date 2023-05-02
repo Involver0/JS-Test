5983a46e0a0f3b9ce08ada803e837b8c4888dc31
Regimantas <regimantasberzinskas@gmail.com>
2023-05-02 09:55:19 +0300

    Task 9- Completed

--- JS Tasks/Task 9/script.js
+++ JS Tasks/Task 9/script.js
@@ -9,3 +9,23 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+    this.wasExpensive = () => {
+      return this.budget > 1e8 ? true : false;
+    };
+  }
+}
+// Example
+
+const lostInTranslation = new Movie(
+  'Lost in translation',
+  'Sofia Coppola',
+  4000000
+);
+console.log(lostInTranslation);
+console.log(lostInTranslation.wasExpensive());
